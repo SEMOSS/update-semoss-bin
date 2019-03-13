a3640fa5e69671bc15c8c265131a225126436ed9
Banach <tbanach@deloitte.com>
2019-03-13 11:18:41 -0400

    update : new options for download etc

--- update_semoss.bat
+++ update_semoss.bat
@@ -1,1 +1,3 @@
+@echo off
 java -Dproperty.file.path="%~dp0update.properties" -Dworking.directory="%~dp0wd" -classpath updatesemoss-0.0.1-SNAPSHOT-jar-with-dependencies.jar org.semoss.updatesemoss.Main
+PAUSE
